d364c62cea98b6bcac01f80c3c785c4dfd59cfd9
LeiZ <52287354+Lei-TzuY@users.noreply.github.com>
2026-09-01 15:44:42 +0800

    Add WHATWG IPv4 loopback referrer regressions

--- tests/referrer_policy_potentially_trustworthy.rs
+++ tests/referrer_policy_potentially_trustworthy.rs
@@ -24,6 +24,42 @@ fn https_to_localhost_is_not_a_strict_referrer_downgrade() {
 }
 
 #[test]
+fn whatwg_ipv4_loopback_spellings_are_trustworthy() {
+    let source = url("https://secure.test/private/path?q=1#secret");
+
+    for target in [
+        "http://127.1/resource",
+        "http://127.0.1/resource",
+        "http://2130706433/resource",
+        "http://0x7f000001/resource",
+        "http://0177.0.0.1/resource",
+        "http://127.0.0.1./resource",
+    ] {
+        assert_eq!(
+            ReferrerPolicy::StrictOriginWhenCrossOrigin.compute(&source, &url(target)),
+            Some("https://secure.test/".to_string()),
+            "WHATWG loopback spelling must remain potentially trustworthy: {target}"
+        );
+    }
+}
+
+#[test]
+fn whatwg_ipv4_loopback_source_to_public_http_is_a_downgrade() {
+    for source in [
+        "http://127.1/private?q=1",
+        "http://2130706433/private?q=1",
+        "http://0x7f000001/private?q=1",
+    ] {
+        assert_eq!(
+            ReferrerPolicy::NoReferrerWhenDowngrade
+                .compute(&url(source), &url("http://public.test/resource")),
+            None,
+            "WHATWG loopback source must suppress referrer toward public HTTP: {source}"
+        );
+    }
+}
+
+#[test]
 fn trustworthy_http_source_to_public_http_is_a_downgrade() {
     for source in [
         "http://localhost/private?q=1",
@@ -59,6 +95,8 @@ fn non_loopback_private_addresses_are_not_implicitly_trustworthy() {
         "http://192.168.1.10/resource",
         "http://10.0.0.8/resource",
         "http://172.16.0.4/resource",
+        "http://0x0a000001/resource",
+        "http://3232235786/resource",
     ] {
         assert_eq!(
             ReferrerPolicy::StrictOriginWhenCrossOrigin.compute(&source, &url(target)),
